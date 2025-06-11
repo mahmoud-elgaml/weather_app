f3abdc2688afb53bc31e83f58f79d90ef46fcdf4
mahmoud tarek <melgamel@std.mans.edu.eg>
2025-06-11 14:54:45 +0300

    Add Colors and create home and searchPage

---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryColor = const Color(0xff000000);
+Color kSecondaryColor =const Color(0xffFFFFFF);
+Color kBackgroundColor = const Color(0xff393E5B);
+Color kTextColor = const Color(0xffEDE6CB);
